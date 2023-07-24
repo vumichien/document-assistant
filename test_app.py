95b891ed1bde18830d866ddc86eec31089ba01a6
vumichien <vumichien1692@gmail.com>
2023-07-24 11:38:20 +0900

    check data test

--- test_app.py
+++ test_app.py
@@ -1,36 +1,18 @@
 import pytest
 import pinecone
 import os
-import importlib
+import glob
 from dotenv import load_dotenv
+from langchain.document_loaders import PyMuPDFLoader
 load_dotenv()
 
-if os.getenv("MODEL_NAME") == "huggingface":
-    dimensions = 384
-else:
-    dimensions = 1536
 
-
-def reset_pinecone() -> None:
-    assert os.getenv("PINECONE_API_KEY") is not None
-    assert os.getenv("PINECONE_ENV") is not None
-
-    import pinecone
-
-    importlib.reload(pinecone)
-
-    pinecone.init(
-        api_key=os.environ.get("PINECONE_API_KEY"),
-        environment=os.environ.get("PINECONE_ENV"),
-    )
-
-
-def test_db():
-    # initialize pinecone
-    reset_pinecone()
-    index = pinecone.Index(os.getenv("PINECONE_INDEX"))
-    index_stats = index.describe_index_stats()
-    assert index_stats["total_vector_count"] != 0
-    assert index_stats["dimension"] == dimensions
-    if index_stats["namespaces"].get(os.getenv("NAME_SPACE")) is not None:
-        assert index_stats["namespaces"].get(os.getenv("NAME_SPACE"))["vector_count"] != 0
+def test_data():
+    pdf_data = []
+    for doc in glob.glob("data/*.PDF"):
+        print(doc)
+        loader = PyMuPDFLoader(doc)
+        loaded_pdf = loader.load()
+        for document in loaded_pdf:
+            pdf_data.append(document)
+    assert len(pdf_data) > 0
